ae66d74844d66f8eb6156cbd2926ed58d08d07f8
Adithya4720 <hegdeadithyak@gmail.com>
2023-10-16 00:29:57 +0530

    Updated Modules,spotify.py,youtube.py

---
+++ python/spotify.py
@@ -0,0 +1,29 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+
+client_id = '535e76588a5b4c6db491cd633213c3ec'
+client_secret = '3ffeca84b6084f47aeb4f2be647b0df0'
+
+# Create a Spotify client
+sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id=client_id, client_secret=client_secret))
+
+# Define the playlist URL
+playlist_url = 'https://open.spotify.com/playlist/37i9dQZF1DWTwbZHrJRIgD?si=42e6b58c9d18453e'
+
+# Function to get song names and artists from a playlist
+def get_playlist_tracks(playlist_url,song_list=[]):
+    # Extract the playlist ID from the URL
+    playlist_id = playlist_url.split('/')[-1]
+    playlist_id = playlist_id.split('?')[0]
+    results = sp.user_playlist_tracks('spotify', playlist_id)
+
+    for track in results['items']:
+        track_info = track['track']
+        track_name = track_info['name']
+        artists = [artist['name'] for artist in track_info['artists']]
+        artist_names = ', '.join(artists)
+        song_list.append((track_name+artist_names))
+    
+
+
---
+++ python/youtube.py
@@ -0,0 +1,8 @@
+from spotify import get_playlist_tracks
+
+#define playlist url
+Playlist_url ="https://open.spotify.com/playlist/37i9dQZF1DWTwbZHrJRIgD?si=42e6b58c9d18453e"
+
+lists=[]
+get_playlist_tracks(Playlist_url,lists)
+
