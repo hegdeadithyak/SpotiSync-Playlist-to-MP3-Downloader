c71b554389abb2e61a60cdfa1aabfca5f7d6fd19
Adithya4720 <hegdeadithyak@gmail.com>
2023-12-06 17:28:27 +0530

    Update FASAPI

--- app.py
+++ app.py
@@ -1,111 +1,18 @@
-import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
 import os
-import urllib.request
-from pytube import YouTube
 import re
 import sys
-
-
-# Get the client_id from environment variables
-client_id = "535e76588a5b4c6db491cd633213c3ec"
-
-client_secret = "3ffeca84b6084f47aeb4f2be647b0df0"
-
-# Create a Spotify client
-sp = spotipy.Spotify(
-    client_credentials_manager=SpotifyClientCredentials(
-        client_id=client_id, client_secret=client_secret
-    )
-)
-
-# Define the playlist URL
-playlist_url = (
-    "https://open.spotify.com/playlist/37i9dQZF1DWTwbZHrJRIgD?si=42e6b58c9d18453e"
-)
-
-
-# Function to get song names and artists from a playlist
-def get_playlist_tracks(playlist_url, song_list=[]):
-    # Extract the playlist ID from the URL
-    playlist_id = playlist_url.split("/")[-1]
-    playlist_id = playlist_id.split("?")[0]
-    results = sp.user_playlist_tracks("spotify", playlist_id)
-
-    for track in results["items"]:
-        track_info = track["track"]
-        track_name = track_info["name"]
-        artists = [artist["name"] for artist in track_info["artists"]]
-        artist_names = ", ".join(artists)
-        song_list.append((track_name + artist_names))
-
-
-def download_audio(video_url):
-    video = YouTube(video_url)
-    audio = video.streams.filter(only_audio=True).first()
-    AUDIO_SAVE_DIRECTORY = sys.argv[2]
-    try:
-        audio.download(AUDIO_SAVE_DIRECTORY)
-    except:
-        print("Failed to download audio")
-
-
-################################Server Side#############################################
-# Define playlist URL
-
-import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
-import os
 import urllib.request
 from pytube import YouTube
-import re
-import sys
-from fastapi import FastAPI, Form, Request
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
+from spotipy.oauth2 import SpotifyClientCredentials
+import spotipy
 
 # Get the client_id from environment variables
 client_id = "535e76588a5b4c6db491cd633213c3ec"
 client_secret = "3ffeca84b6084f47aeb4f2be647b0df0"
-
-# Create a Spotify client
-sp = spotipy.Spotify(
-    client_credentials_manager=SpotifyClientCredentials(
-        client_id=client_id, client_secret=client_secret
-    )
-)
-
-# Define the playlist URL
-playlist_url = (
-    "https://open.spotify.com/playlist/37i9dQZF1DWTwbZHrJRIgD?si=42e6b58c9d18453e"
-)
-
-
-# Function to get song names and artists from a playlist
-def get_playlist_tracks(playlist_url, song_list=[]):
-    # Extract the playlist ID from the URL
-    playlist_id = playlist_url.split("/")[-1]
-    playlist_id = playlist_id.split("?")[0]
-    results = sp.user_playlist_tracks("spotify", playlist_id)
-
-    for track in results["items"]:
-        track_info = track["track"]
-        track_name = track_info["name"]
-        artists = [artist["name"] for artist in track_info["artists"]]
-        artist_names = ", ".join(artists)
-        song_list.append((track_name + artist_names))
-
-
-def download_audio(video_url):
-    video = YouTube(video_url)
-    audio = video.streams.filter(only_audio=True).first()
-    AUDIO_SAVE_DIRECTORY = sys.argv[2]
-    try:
-        audio.download(AUDIO_SAVE_DIRECTORY)
-    except:
-        print("Failed to download audio")
-
 
 # FastAPI configuration
 app = FastAPI()
@@ -118,6 +25,17 @@ class Item(BaseModel):
     playlist_url: str
 
 
+def download_audio(video_url, save_directory):
+    video = YouTube(video_url)
+    audio = video.streams.filter(only_audio=True).first()
+    try:
+        audio.download(save_directory)
+        # Install the downloaded audio file
+        os.system(f"start {os.path.join(save_directory, audio.title)}.webm")
+    except Exception as e:
+        print(f"Failed to download audio: {e}")
+
+
 @app.get("/", response_class=HTMLResponse)
 async def read_item(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
@@ -125,8 +43,28 @@ async def read_item(request: Request):
 
 @app.post("/download", response_class=HTMLResponse)
 async def download_songs(item: Item):
+    # Spotify client setup
+    sp = spotipy.Spotify(
+        client_credentials_manager=SpotifyClientCredentials(
+            client_id=client_id, client_secret=client_secret
+        )
+    )
+
+    # Define the playlist URL
+    playlist_url = item.playlist_url
+
     song_list = []
-    get_playlist_tracks(item.playlist_url, song_list)
+    # Extract the playlist ID from the URL
+    playlist_id = playlist_url.split("/")[-1].split("?")[0]
+    results = sp.user_playlist_tracks("spotify", playlist_id)
+
+    for track in results["items"]:
+        track_info = track["track"]
+        track_name = track_info["name"]
+        artists = [artist["name"] for artist in track_info["artists"]]
+        artist_names = ", ".join(artists)
+        song_list.append((track_name + artist_names))
+
     song_name_lists = [song.replace(" ", "+") for song in song_list]
 
     youtube_url_lists = []
@@ -140,10 +78,10 @@ async def download_songs(item: Item):
 
     for idx, url in enumerate(youtube_url_lists):
         print(f"Downloading {idx + 1}/{len(youtube_url_lists)}: {song_name_lists[idx]}")
-        download_audio(url)
+        download_audio(url, sys.argv[2])
 
     return templates.TemplateResponse(
-        "download.html", {"request": request, "songs": song_name_lists}
+        "download.html", {"request": Request, "songs": song_name_lists}
     )
 
 
